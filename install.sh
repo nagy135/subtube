3a7227e9b803bde5881f6855aebddce798c3280e
nagy135 <viktor.nagy1995@gmail.com>
2019-11-17 18:39:26 +0100

    resources

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+EXECUTABLE_FOLDER=/usr/bin
+INSTALL_DIR=/usr/local/share/subtube
+
+if [[ $1 == '--remove' ]]; then
+    rm $EXECUTABLE_FOLDER/subtube
+    rm -rf $INSTALL_DIR
+else
+
+    mkdir $INSTALL_DIR
+    mkdir $INSTALL_DIR/resources
+
+    cp subtube $EXECUTABLE_FOLDER/subtube
+    chmod +x $EXECUTABLE_FOLDER/subtube
+    cp -r resources/* $INSTALL_DIR/resources
+fi
